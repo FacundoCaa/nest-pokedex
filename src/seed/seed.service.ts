7e4fa09aa6655ef2a59633f7c5c723b8b92dd39c
FacundoCaa <facundocaa@gmail.com>
2025-11-17 20:50:26 -0300

    feat: Initial commit with basic backend functionality

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,17 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');
+    await this.pokemonModel.deleteMany({});
+
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=50');
     const data = (await response.json()) as PokeResponse;
+
+    const pokemonToInsert: { no: number; name: string }[] = [];
 
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
+
+      pokemonToInsert.push({ no, name });
     });
 
-    return data.results;
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+    return 'seed executed';
   }
 }
